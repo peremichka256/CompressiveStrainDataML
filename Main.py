bc699f8b8fa449e679e8f49f37202321dfca7291
Gleb Belous <peremichka256@gmail.com>
2022-10-30 11:52:03 +0700

    Использован декоратор tf.function для увеличения скорости вычислений

--- Main.py
+++ Main.py
@@ -1,17 +1,31 @@
 import os
-
-import keras.utils
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import keras.utils
 import tensorflow as tf
 import keras.datasets.mnist as mnist
 from DenseNN import DenseNN
+from SequentialModule import SequentialModule
 
-def model_predict(x):
-    y = layer_1(x)
-    y = layer_2(y)
-    return y
+
+#Фунция переноса вычислений на уровень графоф
+#(Теперь работает не на уровне интерпритатора Python,
+# а на уровне графогого представления)
+# т.к статические вычисления выполняются один раз, а затем повторяются;
+# вычисления разделяются между потоками и устройствами;
+# общие арифметические операции выполняются один раз.
+@tf.function
+def train_batch(x_batch, y_batch):
+    with tf.GradientTape() as tape:
+        f_loss = cross_entropy(y_batch, model(x_batch))
+
+    # Определяем градиенты
+    grads = tape.gradient(f_loss, model.trainable_variables)
+    # Применяем их к обучаемым параметрам первого и второго слоя
+    opt.apply_gradients(zip(grads, model.trainable_variables))
+
+    return f_loss
+
 
 if __name__ == '__main__':
     # Загрузка данных для обучения
@@ -28,12 +42,7 @@ if __name__ == '__main__':
     # Преобразование в one hot vector
     y_train = keras.utils.to_categorical(y_train, 10)
 
-    # Создание слоев
-    # Первый слой
-    layer_1 = DenseNN(128)
-    # Выходной слой
-    layer_2 = DenseNN(10, activate='softmax')
-
+    model = SequentialModule()
     #Обучение сети(нахождение весовых коэффициентов
     # с помощью градиентного спуска)
     #функция потерь
@@ -54,20 +63,12 @@ if __name__ == '__main__':
         #Суммарное значение потерь
         loss = 0
         for x_batch, y_batch in train_dataset:
-            with tf.GradientTape() as tape:
-                f_loss = cross_entropy(y_batch, model_predict(x_batch))
-
-            loss += f_loss
-            #Определяем градиенты
-            grads = tape.gradient(f_loss, [layer_1.trainable_variables, layer_2.trainable_variables])
-            #Применяем их к обучаемым параметрам первого и второго слоя
-            opt.apply_gradients(zip(grads[0], layer_1.trainable_variables))
-            opt.apply_gradients(zip(grads[1], layer_2.trainable_variables))
+            loss += train_batch(x_batch, y_batch)
 
         print(loss.numpy())
 
     #Определение качества
-    y = model_predict(x_test)
+    y = model(x_test)
     y2 = tf.argmax(y, axis=1).numpy()
     acc = len(y_test[y_test == y2]) / y_test.shape[0] * 100
     print(acc)
