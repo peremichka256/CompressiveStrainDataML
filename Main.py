e31a52c2b013bc028349d4e10e68307e26cb831f
Gleb Belous <peremichka256@gmail.com>
2022-10-31 13:45:07 +0700

    Распознование чисел в Keras

--- Main.py
+++ Main.py
@@ -1,9 +1,51 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from DenseLayer import DenseLayer
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.preprocessing.image import image_utils as image
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    layer1 = DenseLayer(10)
-    y = layer1(tf.constant([[1., 2., 3.]]))
-    print(y)
+def load_image(img_path, show=False):
+
+    img = image.load_img(img_path, target_size=(28, 28))
+    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+
+    if show:
+        plt.imshow(img_tensor[0])
+        plt.axis('off')
+        plt.show()
+
+    return img_tensor
+
+# Get the data as Numpy arrays
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Build a simple model
+inputs = keras.Input(shape=(28, 28))
+x = layers.Rescaling(1.0 / 255)(inputs)
+x = layers.Flatten()(x)
+x = layers.Dense(128, activation="relu")(x)
+x = layers.Dense(128, activation="relu")(x)
+outputs = layers.Dense(10, activation="softmax")(x)
+model = keras.Model(inputs, outputs)
+model.summary()
+
+# Compile the model
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+
+# Train the model for 1 epoch from Numpy data
+batch_size = 64
+print("Fit on NumPy data")
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=1)
+
+# Train the model for 1 epoch using a dataset
+dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(batch_size)
+print("Fit on Dataset")
+history = model.fit(dataset, epochs=1)
+
+predict_image = load_image('8.png', True)
+
+print(x_train)
+print(predict_image)
